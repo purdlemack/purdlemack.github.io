d7630730d06c779ed3a99e4767b0742d58009047
purdlemack <sjpurdie@gmail.com>
2016-09-12 21:57:51 +0100

    Push
    Newest

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#read').click(function () {
+        $('#subhead').html("Get the leads you deserve");
+    });
+    $('#contact').click(function () {
+        $('#subhead').html("Why wait any longer?"),
+
+            $('#contact-email').css("display", "inline");
+
+    });
+})
